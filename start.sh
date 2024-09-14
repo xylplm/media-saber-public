9777e27068e52045120c022966efdf562dc69c80
xylplm <luoxiao123@outlook.com>
2024-09-14 15:08:17 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
-# 检查 PORT 环境变量是否设置，如果没有设置则使用默认端口 80
-if [ -z "$MS_PORT" ]; then
-  MS_PORT=8888
-fi
-
 # 使用环境变量替换配置文件中的占位符
 envsubst '${MS_PORT}' < /etc/nginx/conf.d/site.template > /etc/nginx/conf.d/default.conf
 
