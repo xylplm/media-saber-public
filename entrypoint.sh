e6fbe1774faf0e82a69a754a7f3d781a22448921
DDSRem <73049927+DDSRem@users.noreply.github.com>
2024-09-14 19:04:16 +0800

    fix

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+envsubst "${MS_PORT}" < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
+groupmod -o -g "${PGID}" msaber
+usermod -o -u "${PUID}" msaber
+chown msaber:msaber -R \
+    /app \
+    /var/lib/nginx \
+    /run/nginx \
+    /var/log/nginx \
+    /etc/nginx
+nginx
+cd /app || exit
+umask "${UMASK}"
+exec dumb-init su-exec msaber:msaber /app/mediaSaber
