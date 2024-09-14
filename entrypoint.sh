3bdd1d899ce617e63fc4c95c9f18d6ee5dbd1810
DDSRem <73049927+DDSRem@users.noreply.github.com>
2024-09-14 19:25:32 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-envsubst "${MS_PORT}" < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
+envsubst '${MS_PORT}' < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
 groupmod -o -g "${PGID}" msaber
 usermod -o -u "${PUID}" msaber
 chown msaber:msaber -R \
