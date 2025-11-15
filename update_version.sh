e50fe1a3d87b559705ff5ab51fab8d5c380f1fb0
Jolc <fan_xuankai@126.com>
2025-11-15 23:58:56 +0800

    fix

--- update_version.sh
+++ update_version.sh
@@ -100,8 +100,11 @@ mv "$JSON_FILE.tmp" "$JSON_FILE"
 git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
 
-git add "$JSON_FILE"
+git add .
 git commit -m "Update version: $VERSION"
+git fetch origin main
+git rebase origin/main
 git push origin main
+
 
 echo "版本 $VERSION 已写入 $JSON_FILE 并推送成功"
